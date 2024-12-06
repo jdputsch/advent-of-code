c5b0a68080f6174852208d2199e97bae86911e5c
Jeff Putsch <jputsch@analog.com>
2024-12-05 22:20:18 -0800

    use deltas to determine safety

--- 2024/2.py
+++ 2024/2.py
@@ -1,19 +1,19 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 import pathlib
 import sys
 
+def is_safe(record: list):
+    deltas = [earlier - later for earlier, later in zip(l, l[1:])]
+    mn = min(deltas)
+    mx = max(deltas)
+    return mn > 0 and mx <= 3 or mx < 0 and mn >= -3
+    
 
 with pathlib.Path(sys.argv[1]).open() as f:
     safe = 0
     for line in f.readlines():
         l = list(map(int, line.strip().split()))
-        if all(
-            earlier > later and abs(earlier - later) <= 3
-            for earlier, later in zip(l, l[1:])
-        ) or all(
-            earlier < later and abs(earlier - later) <= 3
-            for earlier, later in zip(l, l[1:])
-        ):
-            safe += 1
+        if is_safe(l):
+             safe += 1
 print(safe)
