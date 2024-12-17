b54d7e375ea42346cd1426667c8e908749dfbe73
Jeff Putsch <jputsch@analog.com>
2024-12-16 21:57:39 -0800

    part 1 solved.

--- 2024/09/soln.py
+++ 2024/09/soln.py
@@ -4,18 +4,41 @@ import pathlib
 import sys
 
 
+def compact_1(disk_blocks):
+    disk_copy = disk_blocks.copy()
+    # compact the disk_blocks
+    # remove trailing "." entries
+    while disk_copy[-1] == ".":
+        disk_copy.pop()
+    while "." in disk_copy:
+        disk_copy[disk_copy.index(".")] = disk_copy.pop()
+        while disk_copy[-1] == ".":
+            disk_copy.pop()
+
+    return disk_copy
+
+
 def main(input_file):
     disk_map = input_file.open().read().strip()
+    disk_blocks = []
     id_no = 0
     map_iterator = iter(disk_map)
-    while pair := tuple(islice(map_iterator, 2)):
-        if len(pair) > 1:
-            blocks, free = pair
-        else:
-            blocks = pair[0]
-            free = 0
-        print(id_no, blocks, free)
+    while pair := list(islice(map_iterator, 2)):
+        if len(pair) == 1:
+            pair.append(0)
+        blocks, free = map(int, pair)
+        for i in range(0, blocks):
+            disk_blocks.append(id_no)
+        for i in range(0, free):
+            disk_blocks.append(".")
         id_no += 1
+
+    compacted_disk = compact_1(disk_blocks)
+    part1_soln = sum(
+        [x * i for x, i in zip(compacted_disk, range(len(compacted_disk)))]
+    )
+
+    print(f"Part 1: {part1_soln}")
 
 
 if __name__ == "__main__":
